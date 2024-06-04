f60893e599bd85315a576c5f0145ba411c715e6e
drag0n1zed <62168687+drag0n1zed@users.noreply.github.com>
2024-06-04 20:03:41 +0800

    initializing part 2

---
+++ src/main/java/drag0n1zed/biologic/BioLogicClient.java
@@ -0,0 +1,10 @@
+package drag0n1zed.biologic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BioLogicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
