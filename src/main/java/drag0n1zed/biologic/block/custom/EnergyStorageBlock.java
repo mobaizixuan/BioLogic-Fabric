2d95aedadf8d69de77bb65f4354511315a8b2928
drag0n1zed <62168687+drag0n1zed@users.noreply.github.com>
2024-06-12 08:05:23 +0800

    fix some things

---
+++ src/main/java/drag0n1zed/biologic/block/custom/EnergyStorageBlock.java
@@ -0,0 +1,9 @@
+package drag0n1zed.biologic.block.custom;
+
+import net.minecraft.block.Block;
+
+public class EnergyStorageBlock extends Block {
+    public EnergyStorageBlock(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/drag0n1zed/biologic/item/ModItems.java
+++ src/main/java/drag0n1zed/biologic/item/ModItems.java
@@ -10,7 +10,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item DNA = registerItem("dna", new Item(new FabricItemSettings()));
     public static final Item PRIMA_MATERIA = registerItem("prima_materia", new Item(new FabricItemSettings()));
-    public static final Item magic_wand = registerItem("magic_wand", new Item(new FabricItemSettings()));
+    public static final Item MAGIC_WAND = registerItem("MAGIC_WAND", new Item(new FabricItemSettings()));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(BioLogic.MOD_ID, name), item);
     }
